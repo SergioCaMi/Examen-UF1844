94e8a5a1b188e1357094e855ccd6093579fd2c01
SergioCaMi <sergiocami84@gmail.com>
2025-05-20 15:07:55 +0200

    add templates

--- index.js
+++ index.js
@@ -1,46 +1,123 @@
+// Server
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+
+// Data
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "data", "images.json");
+
+//Existe el archivo images.son?
+const dirPath = path.join(__dirname, "data");
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+// Leer el contenido del archivo JSON
+let dataImage = [];
+try {
+  const data = fs.readFileSync(filePath, "utf8");
+  dataImage = JSON.parse(data);
+  console.log("Archivo JSON leído correctamente");
+} catch (err) {
+  console.log("Error al leer el archivo JSON:", err);
+}
 
 // Nos permite procesar peticiones POST que vengan de un formulario
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cargamos las vistas EJS
 app.set("view engine", "ejs");
 
-// Oye, si te hacen una petición GET, mira si tienes alguno de los recursos que te piden en el directorio 'public'
+// Carpeta de recursos publicos
 app.use(express.static("public"));
-
-// Array de fotos (base de datos actual)
-const photos = [];
 
 app.get("/", (req, res) => {
   res.send("funciona");
 });
 
+// Add new image
 app.get("/new-image", (req, res) => {
-  // 1. Teneis que renderizar una vista new-image.ejs donde aparezca un formulario para que el usuario rellene los datos que pide la práctica.
-  // Yo implementaré el título y la URL
-  res.render("index.ejs", { title: "Fototeca" });
+  // Mostramos la vista del formulario
+  res.render("addImage.ejs", { title: "New Image" });
 });
 
-// Necesitamos un endpoint donde enviar los datos del formulario. Endpoint donde enviar los datos del formulario
+// Enpoint donde enviamos los datos del formulario
 app.post("/new-image", (req, res) => {
-  // Recibir los datos del formulario y actualizar el array "photos"
-  // Para comprobar que lo has hecho bien muestra por consola el contenido del array photos después de actualizarlo
   console.log("Petición recibida");
   console.log("Body del formulario: ", req.body);
-  if (photos.find((p) => p.urlImagen == req.body.urlImagen)) {
+  // La imagen ya se encuentra en el archivo?
+  if (dataImage.find((p) => p.urlImagen == req.body.urlImagen)) {
+    // SI
     res.send(`La imagen "${req.body.title}" ya se encontraba en el archivo.`);
   } else {
-    photos.push({
+    // NO
+    // Construimos el objeto
+    const newImage = {
       title: req.body.title,
       urlImagen: req.body.urlImagen,
       date: req.body.date,
-    });
-    res.send(`La imagen "${req.body.title}" se ha añadido satisfactoriamente.`);
+    };
+    // Añadimos
+    dataImage.push(newImage);
+    // Lo guardamos en el archivo JSON
+    fs.writeFile(
+      filePath,
+      JSON.stringify(dataImage, null, 2),
+      "utf8",
+      (err) => {
+        err
+          ? res.send(`Error al añadir la imagen "${req.body.title}".`)
+          : res.send(
+              `La imagen "${req.body.title}" se ha añadido satisfactoriamente.`
+            );
+      }
+    );
   }
 });
+
+// Delete  image
+app.get("/delete-image", (req, res) => {
+  // Mostramos la vista del formulario
+  res.render("deleteImage.ejs", { title: "Dfghhgelete Image" });
+});
+
+// Enpoint donde enviamos los datos a eliminar
+app.post("/delete-image", (req, res) => {
+  console.log("Petición recibida");
+  console.log("Body del formulario: ", req.body);
+  // La imagen ya se encuentra en el archivo?
+  if (dataImage.find((p) => p.urlImagen == req.body.urlImagen)) {
+    // SI
+    res.send(`La imagen "${req.body.title}" ya se encontraba en el archivo.`);
+  } else {
+    // NO
+    // Construimos el objeto
+    const newImage = {
+      title: req.body.title,
+      urlImagen: req.body.urlImagen,
+      date: req.body.date,
+    };
+    // Añadimos
+    dataImage.push(newImage.sort((a, b) => a.date - b.date));
+    // Lo guardamos en el archivo JSON
+    fs.writeFile(
+      filePath,
+      JSON.stringify(dataImage, null, 2),
+      "utf8",
+      (err) => {
+        err
+          ? res.send(`Error al añadir la imagen "${req.body.title}".`)
+          : res.send(
+              `La imagen "${req.body.title}" se ha añadido satisfactoriamente.`
+            );
+      }
+    );
+  }
+});
+
 
 // Iniciar el servidor
 app.listen(PORT, () => {
