e03b12450d7074ea5177985c5143d624ffa43f85
SergioCaMi <sergiocami84@gmail.com>
2025-05-21 12:04:15 +0200

    update

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const PORT = 3000;
 // Colores predominantes
 const getColors = require("get-image-colors");
 
+// Id Unicos
+const { v4: uuidv4 } = require("uuid");
+
+//Datos EXIF
+const axios = require("axios");
+const exifReader = require("exif-reader");
+
 // Data
 const fs = require("fs");
 const path = require("path");
@@ -61,71 +68,95 @@ app.get("/new-image", (req, res) => {
 app.post("/new-image", async (req, res) => {
   console.log("Petición recibida");
   console.log("Body del formulario: ", req.body);
-  if (req.body.urlImagen.endsWith(".webp")) {
+  // if (!(req.body.urlImagen.endsWith(".jpg"))||!(req.body.urlImagen.endsWith(".jpeg"))) {
+  //   res.render("addImage.ejs", {
+  //     title: "New Image",
+  //     message: `Formato de imagen no soportado!`,
+  //     colorMessage: "red",
+  //   });
+  // } else {
+  // La imagen ya se encuentra en el archivo?
+  if (dataImage.find((p) => p.urlImagen === req.body.urlImagen)) {
+    // SI
     res.render("addImage.ejs", {
       title: "New Image",
-      message: `Formato de imagen no soportado: WebP`,
+      message: `La imagen "${req.body.title}" ya se encontraba en el archivo.`,
       colorMessage: "red",
     });
   } else {
-    // La imagen ya se encuentra en el archivo?
-    if (dataImage.find((p) => p.urlImagen === req.body.urlImagen)) {
-      // SI
-      res.render("addImage.ejs", {
-        title: "New Image",
-        message: `La imagen "${req.body.title}" ya se encontraba en el archivo.`,
-        colorMessage: "red",
-      });
-    } else {
-      // NO
-      // Construimos el objeto
-      // Obtener los colores de manera asíncrona
+    // NO
+    // Construimos el objeto
+    // Obtener los colores de manera asíncrona
+    try {
       const colors = await getColors(req.body.urlImagen);
-
-      // Construir el objeto newImage con los colores obtenidos
-      const newImage = {
-        title: req.body.title,
-        urlImagen: req.body.urlImagen,
-        date: req.body.date,
-        description: req.body.description,
-        colors: colors,
-      }; // Añadimos
-      dataImage.push(newImage);
-      dataImage.sort((a, b) => new Date(a.date) - new Date(b.date));
-      // Lo guardamos en el archivo JSON
-      fs.writeFile(
-        filePath,
-        JSON.stringify(dataImage, null, 2),
-        "utf8",
-        (err) => {
-          err
-            ? res.render("addImage.ejs", {
-                title: "New Image",
-                message: `Error al añadir la imagen "${req.body.title}".`,
-                colorMessage: "red",
-              })
-            : 
-              res.render("addImage.ejs", {
-                title: "New Image",
-                message: `La imagen "${req.body.title}" se ha añadido satisfactoriamente.`,
-                colorMessage: "green",
-              });
-        }
-      );
+    } catch (error) {
+      console.error("Error al leer los colores de la imagen:", error);
+      colors = null;
     }
-  }
-});
 
+    //Crear ID unico
+    const id = uuidv4();
+
+
+    //Datos EXIF
+
+    // Descargamos la imagen
+    const response = await axios.get(req.body.urlImagen, {
+      responseType: "arraybuffer",
+    });
+    // Pasamos la imagen a binario (buffer)
+    const imageBuffer = Buffer.from(response.data, "binary");
+
+    let exifData;
+    try {
+      exifData = exifReader(imageBuffer);
+    } catch (error) {
+      console.error("Error al leer los datos EXIF:", error);
+      exifData = null;
+    }
+    // Construir el objeto newImage con los colores obtenidos
+    const newImage = {
+      id: id,
+      title: req.body.title,
+      urlImagen: req.body.urlImagen,
+      date: req.body.date,
+      description: req.body.description,
+      colors: colors,
+      exif: exifData,
+    };
+    // Añadimos
+    dataImage.push(newImage);
+    dataImage.sort((a, b) => new Date(a.date) - new Date(b.date));
+    // Lo guardamos en el archivo JSON
+    fs.writeFile(
+      filePath,
+      JSON.stringify(dataImage, null, 2),
+      "utf8",
+      (err) => {
+        err
+          ? res.render("addImage.ejs", {
+              title: "New Image",
+              message: `Error al añadir la imagen "${req.body.title}".`,
+              colorMessage: "red",
+            })
+          : res.render("addImage.ejs", {
+              title: "New Image",
+              message: `La imagen "${req.body.title}" se ha añadido satisfactoriamente.`,
+              colorMessage: "green",
+            });
+      }
+    );
+  }
+  // }
+});
 
 // Enpoint donde enviamos los datos a eliminar
 app.post("/delete-image", (req, res) => {
   console.log("Petición recibida", req.body);
-    res.render("deleteImage.ejs", {
+  res.render("deleteImage.ejs", {
     title: "Delete Image",
     dataImage: req.body,
   });
-
-
 });
 
 // Iniciar el servidor
