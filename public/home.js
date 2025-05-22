f6a0e2206e9d91588e546c2c681dc7c3b12e03c3
SergioCaMi <sergiocami84@gmail.com>
2025-05-22 16:06:21 +0200

    serach date

--- public/home.js
+++ public/home.js
@@ -27,11 +27,13 @@ function showUtils() {
   const searchUtils = document.querySelectorAll(".search");
   const inputSearch = document.querySelector(".searchInput");
   const cards = document.querySelectorAll(".card");
+  const inputDateSearch = document.querySelector(".dateInput");
 
   searchUtils.forEach((element) => {
     if (element.style.display === "flex") {
       element.style.display = "none";
       inputSearch.value = "";
+      showUtils();
     } else {
       element.style.display = "flex";
     }
@@ -52,6 +54,26 @@ function showUtils() {
       } else {
         card.style.display = "flex";
       }
+    });
+
+    inputDateSearch.addEventListener("input", (event) => {
+      const searchDate = new Date(event.target.value);
+      const currentDate = new Date();
+      // Buscamos todas las cards cuya fecha sea menor o igual a la indicada por el user
+      const showCard = dataImage.filter((image) => {
+        const imageDate = new Date(image.date);
+return imageDate >= searchDate && imageDate <= currentDate;
+      });
+      // Recogemos todos los id de las cards a mostrar
+      const showId = showCard.map((image) => image.id);
+      // Dsiplay none a las que no se deben mostrar
+      cards.forEach((card) => {
+        if (!showId.includes(card.id)) {
+          card.style.display = "none";
+        } else {
+          card.style.display = "flex";
+        }
+      });
     });
   });
 }
