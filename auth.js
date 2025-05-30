c92ed1116d94eed12399d061c353660123b7e60f
SergioCaMi <sergiocami84@gmail.com>
2025-05-30 11:44:47 +0200

    update Google OAuth callback URL to production

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ require('dotenv').config(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/google/callback",
+    callbackURL: "https://gallery-2eu4.onrender.com/google/callback",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
